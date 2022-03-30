f61ee6255ebcfcb00cb2d0a34d2391944e3c9976
sam <sam@samfitz.me>
2022-03-30 19:56:07 -0400

    typo

--- src/routes/halloween.jsx
+++ src/routes/halloween.jsx
@@ -1,32 +1,36 @@
 import { Helmet } from "react-helmet";
 
-
 export default function Halloween() {
   return (
-    <div className="main" >
+    <div className="main">
       <Helmet>
         <title>Halloween</title>
       </Helmet>
-      <h1>halloween in april</h1>
-      <p>Everybody loves halloween, why only do it once per year.</p>
+      <h1><a>halloween in april</a></h1>
+      <p>Everybody loves halloween, why only do it only once per year.</p>
       <p>
         Jack and I wanted to host a small halloween themed gathering at our
         place to celebrate half-halloween
       </p>
       <p>
-        our place is at 123 Melrose Street Apartment 957, Bushwick
-        Brooklyn.
+        our place is at 123 Melrose Street Apartment 957, Bushwick Brooklyn.
       </p>
       <p>
         The door people usually buzz people in, but if they don't, buzz
         apartment 957 and we will let you up. Take a right after you go in, take
-        the elevator to floor 9, and take a left down the hallway all the way down to our
-        unit.
+        the elevator to floor 9, and take a left down the hallway all the way
+        down to our unit.
       </p>
-      <p>We will have some drinks and some food, but if you want something specific feel free to byo.</p>
+      <p>
+        We will have some drinks and some food, but if you want something
+        specific feel free to byo.
+      </p>
       <p>costumes are optional</p>
-      <p>at around 11 we are planning to bounce and head to mad tropical, farewell, or one of the many other bushwick bars at our disposal </p>
-      
+      <p>
+        at around 11 we are planning to bounce and head to mad tropical,
+        farewell, or one of the many other bushwick bars at our disposal{" "}
+      </p>
+
       <p>we hope you'll be able to join us.</p>
       <p>sf - jj</p>
       <p>678.621.4871</p>
