c1f20c6c30159d066c0bb9d31bb799e42ad038c0
sam <sam@samfitz.me>
2022-04-04 12:01:40 -0400

    Added Footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <div className='footer'>samfitz.me - built in nyc</div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Footer from './components/footer'
 import About from './routes/about'
 import Blog from './routes/blog'
 
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Route path='about' element={<About />} />
       <Route path='blog' element={<Blog />} />
     </Routes>
+    <Footer />
   </BrowserRouter>,
   document.getElementById('root')
 )
